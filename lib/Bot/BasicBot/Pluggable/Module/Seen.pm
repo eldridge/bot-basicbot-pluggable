19a7e9be35a20400f9cae9eea442c259303edde8
David Precious <davidp@preshweb.co.uk>
2011-11-25 22:37:46 +0000

    Slightly clarify the logic here.
    While I'm here, slightly re-write this logic so the more important parts are
    first, for readability.  (Also, to allow it to short-circuit fastest if we've
    never seen the person in question, but that's not going to make an appreciable
    difference.)

--- lib/Bot/BasicBot/Pluggable/Module/Seen.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Seen.pm
@@ -74,9 +74,9 @@ sub told {
         my $hidden_channel = 
             $seen && exists $ignore_channels->{ $seen->{channel} };
 
-        if ( ( $self->get("user_allow_hiding") and $self->get("hide_$who") )
-            or !$seen or $hidden_channel )
-        {
+        if (!$seen || $hidden_channel
+            || ( $self->get("user_allow_hiding") and $self->get("hide_$who") )
+        ) {
             return "Sorry, I haven't seen $1.";
         }
 
